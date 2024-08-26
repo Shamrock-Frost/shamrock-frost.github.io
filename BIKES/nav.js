37552e6bec3d349bfeccf7d4b00750503be4c14a
Brendan Murpht <shamrockfrost@gmail.com>
2024-08-26 13:25:44 -0600

    Added bikes webpage

---
+++ BIKES/nav.js
@@ -0,0 +1,11 @@
+// Can't we do this in pure HTML/CSS?
+function toggleNav() {
+	if(window.getComputedStyle(document.getElementById("navsymbol")).getPropertyValue("display") !== "none") {
+		var x = document.getElementById("nav");
+		if (x.style.display === "block") {
+			x.style.display = "none";
+		} else {
+			x.style.display = "block";
+		}
+	}
+}
